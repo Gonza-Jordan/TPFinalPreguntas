3e4c076a981b8b3174987e4ae0d0aaa230f18a0b
camilamarendazzo <camimarendazzo@gmail.com>
2024-10-23 13:09:50 -0300

    Suma puntos y redirige al home si es incorrecto

--- app/controller/PartidaController.php
+++ app/controller/PartidaController.php
@@ -11,18 +11,19 @@ class PartidaController
         $this->model = $model;
     }
 
-    public function show() {
+    public function show()
+    {
         SessionHelper::verificarSesion();
         $idUsuario = $_SESSION['user_id']; //Esto es para poder mandarle el usuario al modelo y que le dé una pregunta customizada
-        
-        $data['preguntaActual'] = $this->model->getPregunta();
 
-//        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['respuesta'])) {
-//            $data = $this->validarRespuesta($data, $idUsuario);
-//        } else {
-//            // Si no se envió ninguna respuesta, recargar solo con la pregunta
-//            $data['resultado'] = "";
-//        }
+        $data['pregunta'] = $this->model->getPregunta();
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['respuesta'])) {
+            $data = $this->validarRespuesta($data, $idUsuario);
+        } else {
+            // Si no se envió ninguna respuesta, recargar solo con la pregunta
+            $data['resultado'] = "";
+        }
 
         // Mostrar la vista con los datos
         $this->presenter->show('crearPartida', $data);
@@ -33,11 +34,17 @@ class PartidaController
         $respuestaCorrecta = $data['pregunta']['respuesta_correcta'];
         $respuestaSeleccionada = $_POST['respuesta'];
 
+        echo "Respuesta seleccionada: " . $respuestaSeleccionada . "<br>";
+        echo "Respuesta correcta: " . $respuestaCorrecta . "<br>";
+
         if ($respuestaSeleccionada == $respuestaCorrecta) {
             $this->model->sumarPuntos($idUsuario);
             $data['resultado'] = "correcta";
         } else {
             $data['resultado'] = "incorrecta";
+//            header("Location: home");
+//            exit();
+
         }
 
         $data['respuestaSeleccionada'] = $respuestaSeleccionada;
--- app/model/PartidaModel.php
+++ app/model/PartidaModel.php
@@ -9,13 +9,13 @@ class PartidaModel
     }
 
     public function getPregunta(){
-        $pregunta = $this->conn->query("SELECT * FROM preguntas ORDER BY RAND() LIMIT 1")->fetch(PDO::FETCH_ASSOC);
+        $pregunta = $this->conn->query("SELECT contenido, opcion_a, opcion_b, opcion_c, opcion_d, respuesta_correcta FROM preguntas ORDER BY RAND() LIMIT 1")->fetch(PDO::FETCH_ASSOC);
         return $pregunta;
     }
 
     public function sumarPuntos($idUsuario)
     {
-        $query = "UPDATE usuarios SET puntaje = puntaje + 10 WHERE id_usuario = :id_usuario";
+        $query = "UPDATE usuarios SET puntaje_total = puntaje_total + 10 WHERE id_usuario = :id_usuario";
         $stmt = $this->conn->prepare($query);
         $stmt->bindParam(':id_usuario', $idUsuario);
         $stmt->execute();
